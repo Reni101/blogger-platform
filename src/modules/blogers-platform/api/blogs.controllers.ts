d5524c0c5064dfec0564ff376353954cee6a1638
Maxim D <renigano@gmail.com>
2025-07-05 13:18:22 +0300

    feat: create blog

--- src/modules/blogers-platform/api/blogs.controllers.ts
+++ src/modules/blogers-platform/api/blogs.controllers.ts
@@ -1,14 +1,25 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiParam } from '@nestjs/swagger';
 import { BlogViewDto } from './view-dto/blogs.view-dto';
 import { BlogsQueryRepository } from '../infastructure/query/blogs.query-repository';
+import { CreateBlogInputDto } from './input-dto/blogs.input-dto';
+import { BlogsService } from '../application/blogs.service';
 
 @Controller('blogs')
 export class BlogsControllers {
-    constructor(private blogsQueryRepository: BlogsQueryRepository) {}
+    constructor(
+        private blogsQueryRepository: BlogsQueryRepository,
+        private blogsService: BlogsService,
+    ) {}
     @ApiParam({ name: 'id' })
     @Get(':id')
     async getById(@Param('id') id: string): Promise<BlogViewDto> {
         return this.blogsQueryRepository.getByIdOrNotFoundFail(id);
+    }
+
+    @Post()
+    async createBlog(@Body() body: CreateBlogInputDto): Promise<BlogViewDto> {
+        const blogId = await this.blogsService.createBlog(body);
+        return this.blogsQueryRepository.getByIdOrNotFoundFail(blogId);
     }
 }
--- src/modules/blogers-platform/application/blogs.service.ts
+++ src/modules/blogers-platform/application/blogs.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { CreateBlogDto } from '../dto/create-blog.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Blog, BlogModelType } from '../domain/blog.entity';
+import { BlogsRepository } from '../infastructure/blogs.repository';
 
 @Injectable()
-export class BlogsService {}
+export class BlogsService {
+    constructor(
+        @InjectModel(Blog.name) private BlogModel: BlogModelType,
+        private blogsRepository: BlogsRepository,
+    ) {}
+
+    async createBlog(dto: CreateBlogDto) {
+        const blog = this.BlogModel.createInstance({
+            name: dto.name,
+            description: dto.description,
+            websiteUrl: dto.websiteUrl,
+        });
+
+        await this.blogsRepository.save(blog);
+        return blog._id.toString();
+    }
+}
