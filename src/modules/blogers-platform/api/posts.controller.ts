91a6f42601858c8df137320271c5635529ec2d2f
Maxim D <renissance@tut.by>
2025-07-08 16:55:17 +0300

    feat: get post by id

--- src/modules/blogers-platform/api/posts.controller.ts
+++ src/modules/blogers-platform/api/posts.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreatePostInputDto } from './input-dto/posts.input-dto';
 import { PostsService } from '../application/posts.service';
 import { PostViewDto } from './view-dto/posts.view-dto';
 import { PostsQueryRepository } from '../infastructure/query/posts.query-repository';
+import { ApiParam } from '@nestjs/swagger';
 
 @Controller('posts')
 export class PostsController {
@@ -15,5 +16,11 @@ export class PostsController {
     async createPost(@Body() body: CreatePostInputDto): Promise<PostViewDto> {
         const blogId = await this.postsService.createPost(body);
         return this.postsQueryRepository.getByIdOrNotFoundFail(blogId);
+    }
+
+    @ApiParam({ name: 'id' })
+    @Get(':id')
+    async getById(@Param('id') id: string): Promise<PostViewDto> {
+        return this.postsQueryRepository.getByIdOrNotFoundFail(id);
     }
 }
