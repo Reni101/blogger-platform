c093ec62bff5ce6309923a6c416542c9051861b1
Maxim D <renissance@tut.by>
2025-08-20 20:03:55 +0300

    feat: add validations on blogs

--- src/modules/blogers-platform/api/input-dto/get-blogs-query-params.input-dto.ts
+++ src/modules/blogers-platform/api/input-dto/get-blogs-query-params.input-dto.ts
@@ -1,7 +1,12 @@
 import { BaseQueryParams } from '../../../../core/dto/base.query-params.input-dto';
 import { BlogsSortBy } from './blogs-sort-by';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 export class GetBlogsQueryParams extends BaseQueryParams {
+    @IsEnum(BlogsSortBy)
     sortBy = BlogsSortBy.CreatedAt;
+
+    @IsString()
+    @IsOptional()
     searchNameTerm?: string | null = null;
 }
