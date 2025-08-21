519479c80d70f57facc129c73d92d21cdf0db297
Maxim D <renissance@tut.by>
2025-08-21 11:44:19 +0300

    feat: delete swagger deploy

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { appSetup } from './setup/app.setup';
 import cookieParser from 'cookie-parser';
-import { createWriteStream } from 'fs';
-import { get } from 'http';
 
-const serverUrl = 'http://localhost:3000';
+// const serverUrl = 'http://localhost:3000';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -14,34 +12,34 @@ async function bootstrap() {
     app.use(cookieParser());
     await app.listen(process.env.PORT ?? 3000);
 
-    if (process.env.NODE_ENV === 'development') {
-        // write swagger ui files
-        get(`${serverUrl}/swagger/swagger-ui-bundle.js`, function (response) {
-            response.pipe(
-                createWriteStream('swagger-static/swagger-ui-bundle.js'),
-            );
-        });
-
-        get(`${serverUrl}/swagger/swagger-ui-init.js`, function (response) {
-            response.pipe(
-                createWriteStream('swagger-static/swagger-ui-init.js'),
-            );
-        });
-
-        get(
-            `${serverUrl}/swagger/swagger-ui-standalone-preset.js`,
-            function (response) {
-                response.pipe(
-                    createWriteStream(
-                        'swagger-static/swagger-ui-standalone-preset.js',
-                    ),
-                );
-            },
-        );
-
-        get(`${serverUrl}/swagger/swagger-ui.css`, function (response) {
-            response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
-        });
-    }
+    // if (process.env.NODE_ENV === 'development') {
+    //     // write swagger ui files
+    //     get(`${serverUrl}/swagger/swagger-ui-bundle.js`, function (response) {
+    //         response.pipe(
+    //             createWriteStream('swagger-static/swagger-ui-bundle.js'),
+    //         );
+    //     });
+    //
+    //     get(`${serverUrl}/swagger/swagger-ui-init.js`, function (response) {
+    //         response.pipe(
+    //             createWriteStream('swagger-static/swagger-ui-init.js'),
+    //         );
+    //     });
+    //
+    //     get(
+    //         `${serverUrl}/swagger/swagger-ui-standalone-preset.js`,
+    //         function (response) {
+    //             response.pipe(
+    //                 createWriteStream(
+    //                     'swagger-static/swagger-ui-standalone-preset.js',
+    //                 ),
+    //             );
+    //         },
+    //     );
+    //
+    //     get(`${serverUrl}/swagger/swagger-ui.css`, function (response) {
+    //         response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
+    //     });
+    // }
 }
 bootstrap();
