da325e808887a7bcad3fd09709bb9e5e5bb44de5
Maxim D <renissance@tut.by>
2025-08-21 09:35:09 +0300

    feat:blogs input validation

--- src/modules/blogers-platform/api/input-dto/blogs.input-dto.ts
+++ src/modules/blogers-platform/api/input-dto/blogs.input-dto.ts
@@ -1,18 +1,47 @@
 import { CreateBlogDto } from '../../dto/create-blog.dto';
+import { IsEmail, IsString } from 'class-validator';
+import { Trim } from '../../../../core/decorators/transform/trim';
 
 export class CreateBlogInputDto implements CreateBlogDto {
+    @IsString()
+    @Trim()
     name: string;
+
+    @IsString()
+    @Trim()
     description: string;
+
+    @IsString()
+    @IsEmail()
+    @Trim()
     websiteUrl: string;
 }
 
 export class UpdateBlogInputDto {
+    @IsString()
+    @Trim()
     name: string;
+
+    @IsString()
+    @Trim()
     description: string;
+
+    @IsString()
+    @IsEmail()
+    @Trim()
     websiteUrl: string;
 }
 export class CreatePostByBlogIdInputDto {
+    @IsString()
+    @Trim()
     title: string;
+
+    @IsString()
+    @Trim()
     shortDescription: string;
+
+    @IsString()
+    @Trim()
+    s;
     content: string;
 }
